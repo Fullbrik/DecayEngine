94e66159cb77d6f2849c15802fd44f0acd23ebcf
mcp613 <support@fullbrik.com>
2021-05-19 15:49:55 -0700

    Deleted gitignore files

--- modules/misc/example/target/debug/deps/example.d
+++
@@ -1,7 +0,0 @@
-/Users/tuviafrankel/Documents/GitHub/DecayEngine/modules/misc/example/target/debug/deps/libexample.dylib: src/lib.rs
-
-/Users/tuviafrankel/Documents/GitHub/DecayEngine/modules/misc/example/target/debug/deps/libexample.rlib: src/lib.rs
-
-/Users/tuviafrankel/Documents/GitHub/DecayEngine/modules/misc/example/target/debug/deps/example.d: src/lib.rs
-
-src/lib.rs:
--- modules/misc/example/target/debug/libexample.d
+++
@@ -1,1 +0,0 @@
-/Users/tuviafrankel/Documents/GitHub/DecayEngine/modules/misc/example/target/debug/libexample.dylib: /Users/tuviafrankel/Documents/GitHub/DecayEngine/modules/misc/example/src/lib.rs
